b9708ca2657e5f5b8b197a2b9d69bae9ccd9075d
Arnab <arnabdnri@gmail.com>
2024-06-05 15:59:59 +0530

    Added animation and styles to personal projects

--- src/contents/projects/Projects.jsx
+++ src/contents/projects/Projects.jsx
@@ -1,7 +1,72 @@
 import memory from '../../assets/pokemon-memory.png';
 import tictactoe from '../../assets/tic-tac-toe.png';
+import React from 'react';
 
 function Projects() {
+  const handleImageClick = (e) => {
+    if (!e.target.classList.contains('moveTop')) {
+      e.target.classList.toggle('moveTop');
+    }
+  };
+
+  const handleDescClick = (e) => {
+    if (
+      e.target.classList.contains('line') ||
+      e.target.classList.contains('projectLink') ||
+      e.target.classList.contains('projectPara')
+    ) {
+      if (
+        e.target.parentNode.parentNode.childNodes[0].classList.contains(
+          'moveTop'
+        )
+      ) {
+        e.target.parentNode.parentNode.childNodes[0].classList.toggle(
+          'moveTop'
+        );
+      }
+    } else if (e.target.classList.contains('link')) {
+      if (
+        e.target.parentNode.parentNode.parentNode.childNodes[0].classList.contains(
+          'moveTop'
+        )
+      ) {
+        e.target.parentNode.parentNode.parentNode.childNodes[0].classList.toggle(
+          'moveTop'
+        );
+      }
+    } else {
+      if (e.target.parentNode.childNodes[0].classList.contains('moveTop')) {
+        e.target.parentNode.childNodes[0].classList.toggle('moveTop');
+      }
+    }
+  };
+
+  React.useEffect(() => {
+    const images = document.querySelectorAll('.projectImage');
+    for (let i = 0; i < images.length; i++) {
+      images[i].addEventListener('click', handleImageClick);
+    }
+
+    return () => {
+      for (let i = 0; i < images.length; i++) {
+        images[i].removeEventListener('click', handleImageClick);
+      }
+    };
+  }, []);
+
+  React.useEffect(() => {
+    const desc = document.querySelectorAll('.projectDescription');
+    for (let i = 0; i < desc.length; i++) {
+      desc[i].addEventListener('click', handleDescClick);
+    }
+
+    return () => {
+      for (let i = 0; i < desc.length; i++) {
+        desc[i].removeEventListener('click', handleDescClick);
+      }
+    };
+  }, []);
+
   return (
     <section>
       <h3>Software Projects</h3>
@@ -32,15 +97,15 @@ function Projects() {
 function SoftwareProject({ screen, alt, desc, github, site }) {
   return (
     <div className="project">
-      <img src={screen} alt={alt} />
+      <img src={screen} alt={alt} className="projectImage" />
       <div className="projectDescription">
-        <p>{desc}</p>
+        <p className="projectPara">{desc}</p>
         <div className="line"></div>
         <div className="projectLink">
-          <a href={github} target="_blank">
+          <a href={github} target="_blank" className="link">
             GitHub
           </a>
-          <a href={site} target="_blank">
+          <a href={site} target="_blank" className="link">
             Live Site
           </a>
         </div>
